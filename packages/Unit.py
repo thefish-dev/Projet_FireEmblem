febcdf1fa6422836444cf6dd6862f203c0acb3eb
Demet <demetlegal@gmail.com>
2023-10-02 10:26:45 +0200

    heth

--- packages/Unit.py
+++ packages/Unit.py
@@ -1,4 +1,5 @@
 from .Player import Player
+from .Attack import Attack
 
 class Unit:
     def __init__(self, name: str, unitType: str, maxHealth: int, attacks: list, abilities: (list or None), properties: (list or None)):
@@ -11,7 +12,7 @@ class Unit:
 
         self.health = self.maxHealth
 
-    def setParent(self, parent: Player):
+    def SetParent(self, parent: Player):
         self.parent = parent
 
     def ModifyHealth(self, modification: int):
@@ -21,3 +22,12 @@ class Unit:
             self.health = 0
         else:
             self.health += modification
+
+    def IsAlive(self):
+        return self.health > 0
+    
+    def Attack(self, unit, attack: Attack):
+        unit.ModifyHealth(attack.damages)
+        ans1 = f"{unit.name} has been attacked and received {attack.damages} damages."
+        ans2 = f"{unit.name} has been killed."
+        print(ans1 if unit.IsAlive() else ans2)        
