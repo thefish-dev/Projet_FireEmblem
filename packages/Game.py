a54e0f65896c69f39b81194371751538c7ab72fc
Louis <louis.brynildsvoll@gmail.com>
2023-10-03 17:14:31 +0200

    game

--- packages/Game.py
+++ packages/Game.py
@@ -1,12 +1,14 @@
 
 class Game:
-    def __init__(self, size):
+    def __init__(self, size:int):
         self.grid = [ [' ' for i in range (size[0])] * size[1] ]
         
     def affichage(self) :
+        for i in range(len(self.grid)) :
+            print(self.grid[i])
         
     def PlaceUnit(self, unit, position):
         self.grid[position[0]][position[1]]=unit.name
         
     def MoveUnit(self, unit, position):
-        ...
+        
