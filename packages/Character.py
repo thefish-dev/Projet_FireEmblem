e72e61254a4fff90e9c8e0736d30aac580de7c1d
Demet <demetlegal@gmail.com>
2023-10-03 17:06:14 +0200

    jgrzi

---
+++ packages/Character.py
@@ -0,0 +1,5 @@
+from .Unit import Unit
+
+class Character:
+    def __init__(self, name: str, units: list):
+        pass
--- packages/Player.py
+++ packages/Player.py
@@ -1,8 +1,9 @@
+from .Character import Character
 
 class Player:
-    def __init__(self, units, character):
-        self.units = units
+    def __init__(self, character):
         self.character = character
+        self.units = self.character.units
         
     def CanPlay(self):
         return len(self.units) > 0
