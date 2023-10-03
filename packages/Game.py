b62a14189e3b8e4d333d0a97675cc9b671e9b016
Louis <louis.brynildsvoll@gmail.com>
2023-10-03 17:56:04 +0200

    vb

--- packages/Game.py
+++ packages/Game.py
@@ -1,14 +1,24 @@
-
+from math import *
 class Game:
     def __init__(self, size:int):
         self.grid = [ [' ' for i in range (size[0])] * size[1] ]
         
-    def affichage(self) :
+    def Affichage(self) :
         for i in range(len(self.grid)) :
             print(self.grid[i])
+
+
+    def KnowPosition(self) :
+        for i in range (len(self.grid)) :
+            for j in range(len(self.grid[i])) :
+                if self.grid([i][j])!=' ' :
+                    return (i,j)
+
+    def Distance(self,position:tuple,position_voulue:tuple) :
+        return sqrt(((position_voulue[0]-position[0])**2)+((position_voulue[1]-position[1])**2))
         
     def PlaceUnit(self, unit, position):
-        self.grid[position[0]][position[1]]=unit.name
+        self.grid[position[0]][position[1]]=unit 
         
-    def MoveUnit(self, unit, position):
-        
+    def MoveUnit(self, unit,position:tuple, position_voulue :tuple):
+        if self.distance(position,position_voulue)>...
