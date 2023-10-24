91031c2895679be28cc9dee0aa76b80e5d3c137b
Louis <louis.brynhildsvoll@gmail.com>
2023-10-24 22:21:30 +0200

    b

--- Pygame.py
+++ Pygame.py
@@ -6,18 +6,23 @@ pygame.init()
 
 screen = pygame.display.set_mode([800, 800])
 picture=pygame.image.load('./Image/Cadrillage.png')
-
+picture_use=picture.convert
 running = True
 while running:
+    screen.fill((255, 255, 255))
+    picture.blit(picture_use,(0,0))
 
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
     
     screen.fill((255, 255, 255))
+    picture.blit(picture_use,(0,0))
+           
 
+    
+   
     
     pygame.display.flip()
 
