dfc1b7d4206ebcebee44f931dd64bd863536ae26
Louis <louis.brynhildsvoll@gmail.com>
2023-10-26 21:20:12 +0200

    uju

--- Pygame.py
+++ Pygame.py
@@ -1,25 +1,31 @@
 from pygame import *
+ 
+background = display.set_mode((800,800))
+display.set_caption('Fire emblem')
+init()
+ 
+#les images
+picture = image.load("./Images/Cadrillage.png")
+picture = picture.convert_alpha()
+picture=transform.scale(picture, (600, 600))
 
-import pygame
-pygame.init()
+arbre = image.load("./Images/arbre.PNG")
+arbre = arbre.convert_alpha()
+arbre=transform.scale(arbre, (100, 100))
 
-sources_path = "./Images/"
-screen = pygame.display.set_mode([800, 800])
-picture=pygame.image.load(sources_path +"Cadrillage.png")
-picture_use=picture.convert
-running = True
-while running:
-    screen.fill((255, 255, 255))
-    picture.blit(picture_use,(0,0))
-
-    
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    
-    
-    
-    pygame.display.flip()
-
-
-pygame.quit()
+#les spritesheets
+        
+#paramètres de départ
+jouer = True
+ 
+#les fonctions du jeu
+ 
+while jouer:
+    for events in event.get():
+         if events.type == QUIT:
+             jouer=False
+             quit()
+    background.blit(picture, (100,100))
+    background.blit(arbre, (0,100))
+    display.flip()
+quit()
