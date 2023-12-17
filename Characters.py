6592dac756671016e723a9c9bf5e4703b6881fe2
TheFish <demetlegal@gmail.com>
2023-12-17 12:44:39 +0100

    changements

--- Characters.py
+++ Characters.py
@@ -4,34 +4,34 @@ Characters = [
     Character("Urart", [ # list of Unit instances
         Unit(name="Minibombe", unitType="Feu", maxHealth=80, maxDistance=3, 
         attacks=[ # list of Attack instances
-            Attack("Explosion Grotesque", "Cette attaque est une attaque trés forte mais l'unité minibombe meurre lors de l'attaque", 90),
-            Attack("C4", "Cette attaque peut s'utiliser à distance (de 3 cases max) car la minibombe lance un C4 sur son adversaire ", 30),
+            Attack(name="Explosion Grotesque", desc="Cette attaque est une attaque trés forte mais l'unité minibombe meurre lors de l'attaque", damages=90, range=2),
+            Attack("C4", "Cette attaque peut s'utiliser à distance (de 3 cases max) car la minibombe lance un C4 sur son adversaire ", 30, 3),
         ],
         abilities=[ # list of Ability instances
-            Ability("Protéger", "Peut protéger")
+            Ability(name="Protéger", desc="Peut protéger", max_uses=1)
         ]
         ),
         Unit("Ouantit_the_wizard", "Magie", 100, 5, 
         attacks=[
-            Attack("Coup de foudre", "Cette attaque electrocute une unité, vous pouvez l'utiliser à une distance de 2 case max", 20)
+            Attack("Coup de foudre", "Cette attaque electrocute une unité, vous pouvez l'utiliser à une distance de 2 case max", 20, 2)
         ],
         abilities=[
-            Ability("Vol", "Vous pouvez volez aléatoirement une attaque de n'importe quelle unité que vous pourrez utiliser au prochain tour UNIQUEMENT.")
+            Ability("Vol", "Vous pouvez volez aléatoirement une attaque de n'importe quelle unité que vous pourrez utiliser au prochain tour UNIQUEMENT.", 1)
         ]
         )
     ]),
     Character("Metde", [
         Unit("Singe mangeur de viande", "Viandard", 110, 5, 
         attacks=[
-            Attack("Coup de chico super chokbar", "Le singe vous mord si fort que l'adversaire hurle à la mort", 25),
-            Attack("Coup de pied retourner en highkick", "Cette attaque est trés puissante et fait donc beaucoup de dégats", 75),
+            Attack("Coup de chico super chokbar", "Le singe vous mord si fort que l'adversaire hurle à la mort", 30, 1),
+            Attack("Coup de pied retourner en highkick", "Cette attaque est trés puissante !", 75, 1),
         ],
         abilities=None
         ),
         Unit("Dustin Poirier", "Punch", 60, 5, 
         attacks=[
-            Attack("Ground and pound", "Cette attaque est agressive et puissante mais elle necessite d'étre proche pour étre utiliser (1 case)", 35),
-            Attack("étranglement en guillotine", "ataque trés puisante , il ne faut pas mettre en rage dustin Poirier", 60)
+            Attack("Ground and pound", "Cette attaque est agressive et puissante mais elle necessite d'étre proche pour étre utiliser (1 case)", 35, 1),
+            Attack("étranglement en guillotine", "ataque trés puisante , il ne faut pas mettre en rage dustin Poirier", 60, 1)
         ],
         abilities=None
         )
@@ -39,20 +39,20 @@ Characters = [
     Character("Godi", [
         Unit("Stone", "Pierre", 200, 5, 
         attacks=[
-            Attack("Charge", "Il te fonce dedans avec son gros crane de pierre ", 25),
-            Attack("Tremblement de terre", "Il peut faire trembler le sol et ainsi infliger des dégats", 20),
+            Attack("Charge", "Il te fonce dedans avec son gros crane de pierre ", 25, 2),
+            Attack("Tremblement de terre", "Il peut faire trembler le sol et ainsi infliger des dégats", 20, 2),
         ],
         abilities=[
-            Ability("Protéger", "Il lance une pierre devant un de ses collégues, ce qui empêche toute attaque")
+            Ability("Protéger", "Il lance une pierre devant un de ses collégues, ce qui empêche toute attaque", 1)
         ]
         ),
         Unit("Serpent ", "feuille", 60, 5, 
         attacks=[
-            Attack("Etranglement en guillotine", "Le serpent étrangle l'unité.", 35),
-            Attack("Morsure", "Le serpent tempoisonne, l'unité perd alors 20 pv par tour", 20),
+            Attack("Etranglement en guillotine", "Le serpent étrangle l'unité.", 35, 1),
+            Attack("Morsure", "Le serpent tempoisonne, l'unité perd alors 20 pv par tour", 20, 1),
         ],
         abilities=[
-            Ability("Soin","Ce serpent est aussi un serpent radioactif, il peut alors soigner de 20 pv une de vos unités")
+            Ability("Soin","Ce serpent est aussi un serpent radioactif, il peut alors soigner de 20 pv une de vos unités", 2)
         ]
         )
     ])
--- packages/Ability.py
+++ packages/Ability.py
@@ -1,15 +1,69 @@
-from .Abilities import abilities
+import random
+
+def heal(author, target):
+    target.modify_health(20)
+    return f"Vous avez soigné {target.name}"
+
+def protect(author, target):
+    target.shield = 1
+    return f"Vous avez protégé {target.name}" 
+
+def shield(target):
+    target.shield = 1
+    return "Vous vous êtes appliqué un bouclier. Vous pourrez désormais résistez à la prochaine attaque."
+
+def blind(author, target):
+    target.can_attack = 1
+    return f"{target.name} est maintenant aveuglé, il ne pourra plus attaquer."
+
+def rob(author, target):
+    attacks = target.attacks
+    random_attack = random.choice(attacks)
+
+    attacks.remove(random_attack)
+    author.attacks += [random_attack]
+
+abilities = {
+    "Soin": {
+        "target": "ally",
+        "effect": heal
+    },
+    "Protéger": {
+        "target": "ally",
+        "effect": protect
+    },
+    "Bouclier": {
+        "target": "self",
+        "effect": shield
+    },
+    "Aveugler": {
+        "target": "enemy",
+        "effect": blind
+    },
+    "Vol": {
+        "target": "enemy",
+        "effect": rob
+    },
+}
+
+
 
 class Ability:
-    def __init__(self, name: str, desc: str):
+    def __init__(self, name: str, desc: str, max_uses: int):
         self.name = name
         self.desc = desc
+        self.max_uses = max_uses
+
+        self.uses = 0
 
         self.ability = abilities[self.name] 
         self.target = self.ability["target"]
         self.effect = self.ability["effect"]
 
     def Run(self, author, target):
+        if self.uses >= self.max_uses:
+            return "Tu as utilisé cette abilité trop de fois, elle est désormais indisponible."
+
         if self.target == "ally":
             if target.team == author.team:
                 return self.effect(author, target)
